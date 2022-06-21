4bee6040eda67fcc62a09ef86b4bf9cb1f6e8a54
Vangelis Tripolitakis <vtri@qnr.com.gr>
2022-06-21 18:26:28 +0300

    [CHG] test...

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.tripolitakis = exports.vaggelis = void 0;
+var vaggelis = function () { return 'Vaggelis'; };
+exports.vaggelis = vaggelis;
+var tripolitakis = function () { return 'Tripolitakis'; };
+exports.tripolitakis = tripolitakis;
